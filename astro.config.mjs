bd5f3d5a5b764b495ce18dbb1297020a0e89908d
李兴广 <lixingguang@xunlei.com>
2023-10-21 09:46:13 +0800

    feat: 增加mdx支持

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import solid from "@astrojs/solid-js";
 import UnoCSS from "unocss/astro";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,6 @@ export default defineConfig({
     UnoCSS({
       injectReset: true,
     }),
+    mdx()
   ],
 });
